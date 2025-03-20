dd1baf07bee999d15313179a1adffaac5c6dcfba
Thao Vo <thaovo@MacBook-Pro-3.local>
2025-03-19 22:53:23 -0700

    restructure, added global styles and variables, laid out nav components

---
+++ src/sections/homeHero.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function homeHero() {
+  return (
+    <div className='container-outer'>
+      <div className='container'>
+       
+      </div>
+    </div>
+  );
+}
+
+export default homeHero;
