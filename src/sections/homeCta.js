68b09ee7eda7dff87d4f713490670fff728bbacf
Thao Vo <thaovo@MacBook-Pro-3.local>
2025-03-28 11:06:42 -0700

    contact button in CTA section updated

--- src/sections/homeCta.js
+++ src/sections/homeCta.js
@@ -8,7 +8,7 @@ function homeCta() {
         <div className="home-cta-content">
           <h2 className="dark">Pause!</h2>
           <p>Don't let another moment slip away—book your session today!</p>
-          <button>contact me</button>
+          <a className="home-cta-btn" href="/contact">contact me</a>
         </div>
       </div>
     </div>
